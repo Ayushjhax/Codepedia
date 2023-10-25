7003d0c78d4787e0d7de6f1b05a7635b7d9ccacf
Ayush kumar jha <ayushkmrjha@yahoo.com>
2023-10-25 09:02:08 +0530

    Update 3Sum.cpp
    Reduced duplicate work

--- Problems/3Sum.cpp
+++ Problems/3Sum.cpp
@@ -1,31 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
         vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+        int n = nums.size();
+        if (n < 3) return output;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int target = -nums[i];
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
                 if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
+                    output.push_back({nums[i], nums[left], nums[right]});
+                    // Skip duplicate elements
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
                 } else if (sum < target) {
-                    j++;
+                    left++;
                 } else {
-                    k--;
+                    right--;
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
+
         return output;
     }
 };
